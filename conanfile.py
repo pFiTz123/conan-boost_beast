128515574cd2bc27e9094d2c5048a753f4c40fa7
Jerry W <jerrywiltse@gmail.com>
2017-10-13 14:50:30 -0400

    working commit

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ from conans import ConanFile, tools, os
 class BoostBeastConan(ConanFile):
     name = "Boost.Beast"
     version = "20171013"
+    commit_id = "f09b2d3e1c9d383e5d0f57b1bf889568cf27c39f"
     url = "https://github.com/bincrafters/conan-boost-beast"
     description = "Boost.beast provides HTTP and WebSocket built on Boost.Asio in C++11"
     license = "www.boost.org/users/license.html"
@@ -14,7 +15,9 @@ class BoostBeastConan(ConanFile):
     def source(self):
         source_url = "https://github.com/boostorg/beast"
         self.run("git clone --depth=1 --branch=master {0}.git".format(source_url))
-
+        with tools.chdir("./beast"):
+            self.run("git checkout {0}".format(self.commit_id))
+            
     def package(self):
         for lib_short_name in self.lib_short_names:
             include_dir = os.path.join(lib_short_name, "include")
